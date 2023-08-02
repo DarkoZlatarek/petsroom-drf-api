70f4c46354ca025b63fe817b93094ec547350550
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-02 20:48:15 +0000

    Add ProfileDetail class in views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -12,4 +13,22 @@ class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
+# Class taken from DRF-API walkthrough.
+class ProfileDetail(APIView):
+    """
+    Retrieve or update Profile fields if Owner.
+    """
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
         return Response(serializer.data)
