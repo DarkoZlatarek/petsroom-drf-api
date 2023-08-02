8e8b1a86fdd31de664fd311d394b49300ec09a5b
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-02 20:57:22 +0000

    Add edit method in ProfileDetails view

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -21,14 +22,24 @@ class ProfileDetail(APIView):
     """
     Retrieve or update Profile fields if Owner.
     """
+    serializer_class = ProfileSerializer
+
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
             return profile
         except Profile.DoesNotExist:
             raise Http404
-    
+
     def get(self, request, pk):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
