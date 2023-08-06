16bc6c09fb975b2219394234c8177639a3a2ab29
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-06 10:29:39 +0000

    Add serializers for Posts

---
+++ posts/serializers.py
@@ -0,0 +1,81 @@
+from rest_framework import serializers
+from likes.models import Like
+from .models import Post
+
+
+# Class taken from DRF-API walkthrough.
+class PostSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Posts Model.
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
+
+    def validate_image(self, value):
+        """
+        Restrict size of image upload.
+        Produce error warning to inform user.
+        """
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError(
+                'Image must be smaller than 2MB.')
+
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Height must be smaller than 4096px.')
+
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Width must be smaller than 4096px.')
+
+        return value
+
+    def get_is_owner(self, obj):
+        """
+        Return correct user.
+        """
+        request = self.context['request']
+        return request.user == obj.owner
+
+    def get_like_id(self, obj):
+        """
+        Return & calculate total number
+        of likes on post view.
+        """
+        user = self.context['request'].user
+
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user,
+                post=obj
+            ).first()
+            return like.id if like else None
+
+        return None
+
+    class Meta:
+        """
+        Display fields for views.
+        """
+        model = Post
+        fields = [
+            'id',
+            'owner',
+            'is_owner',
+            'profile_id',
+            'profile_image',
+            'created_on',
+            'modified_on',
+            'title',
+            'content',
+            'image',
+            'dog_breed',
+            'like_id',
+            'likes_count',
+            'comments_count',
+        ]
