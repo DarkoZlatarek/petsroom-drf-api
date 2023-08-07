41236f48ad6badfd49957394780251a434e2d6f3
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-07 15:46:35 +0000

    Add views, serializers and urls for Likes

---
+++ likes/serializers.py
@@ -0,0 +1,33 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from likes.models import Like
+
+
+# Class provided by DRF-API walkthrough.
+class LikeSerializer(serializers.ModelSerializer):
+    """
+    Likes model serializer.
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = [
+            'id',
+            'created_on',
+            'owner',
+            'post',
+        ]
+
+    def create(self, validated_data):
+        """
+        Throws error message if
+        the user tries to like the same
+        post more than once.
+        """
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from p5_drf_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
 
-# Create your views here.
+
+# Class provided by DRF-API walkthrough.
+class LikeList(generics.ListCreateAPIView):
+    """
+    Logged in users is able
+    to like/unlike a post.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        """
+        Saves like or unlike to the database,
+        if the user is authenticated.
+        """
+        serializer.save(owner=self.request.user)
+
+
+# Class provided by DRF-API walkthrough.
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    The ability to create/delete
+    a like.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
