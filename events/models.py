9a629e8c9c1dae6d2f7aeed237cf2c17e8bf5b1a
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-08 19:35:12 +0000

    Add DEBUG=True, events app and the models for it

---
+++ events/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Event(models.Model):
+    """
+    Events model for database.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    place = models.CharField(max_length=50)
+    content = models.TextField(blank=True)
+    date = models.DateField(null=True, blank=True)
+    time = models.TimeField(null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """
+        Order events by date of event,
+        soonest first.
+        """
+        ordering = ['-date']
+
+    def __str__(self):
+        return f'{self.title} {self.date}'
