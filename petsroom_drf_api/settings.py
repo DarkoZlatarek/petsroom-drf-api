23028f25dba00b4ca79ef3b949a550b554587e83
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-08-20 07:46:42 +0000

    Update DEBUG back to False

--- petsroom_drf_api/settings.py
+++ petsroom_drf_api/settings.py
@@ -60,11 +60,11 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
    os.environ.get('ALLOWED_HOST'),
-   '*',
+   'localhost',
 ]
 
 # Application definition
